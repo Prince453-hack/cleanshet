4fb10384ae26bab40733e91387628dfd4a09460e
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-07-11 13:21:21 +0530

    ui changes

--- src/components/OurMission.tsx
+++ src/components/OurMission.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+//@ts-expect-error
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const OurMission = () => {
+const OurMission: React.FC = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
-    <div className="my-16">
-      <h1 className="text-center font-bold text-5xl text-[#331846]">
+    <div className="my-24 px-4 lg:px-0">
+      <h1
+        className="text-center font-bold text-3xl lg:text-5xl text-[#331846]"
+        data-aos="fade-up"
+      >
         Our Mission
       </h1>
-      <p className="text-4xl my-10 max-w-5xl mx-auto text-center font-semibold border-2 border-[#331846] rounded-3xl py-6 px-5">
+      <p
+        className="text-xl lg:text-4xl my-14 max-w-5xl mx-auto text-center font-semibold border-2 border-[#331846] rounded-3xl py-6 px-5"
+        data-aos="fade-up"
+      >
         "We envision a future of Zero waste intimate paper products. Committing
         to minimize the impact of our shifts to the world"
       </p>
--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -2,44 +2,43 @@ import Image from "next/image";
 import React, { useEffect } from "react";
 //@ts-expect-error
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 
-const MissionStatement: React.FC = () => {
+const WhyUs: React.FC = () => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
 
   return (
-    <div className="relative flex flex-col md:flex-row items-center justify-center p-8 mb-10">
-      <div
-        className="absolute top-0 left-1/2 transform -translate-x-1/2 md:left-1/4 md:translate-x-0 bg-purple-100 rounded-tl-lg rounded-bl-lg w-full h-full md:w-3/4"
-        style={{ zIndex: 0 }}
-      ></div>
-      <div
-        className="relative flex flex-col md:flex-row items-center gap-8"
-        style={{ zIndex: 1 }}
-      >
+    <div className="flex flex-col items-center justify-center bg-purple-100 p-10">
+      <div className="flex flex-col lg:flex-row p-4 items-center lg:items-start">
         <div
-          className="bg-purple-900 text-white p-10 h-64 rounded-lg z-10 text-center flex flex-col items-center shadow-md"
+          className="flex-shrink-0 lg:ml-36 bg-purple-900 h-auto lg:h-[20rem] text-white p-6 shadow-md rounded-lg w-full lg:w-auto"
+          data-aos="fade-right"
+        >
+          <div className="text-lg font-bold mx-auto">
+            <Image
+              src="/logo.png"
+              width={300}
+              height={300}
+              alt="CleanShet"
+              className="mx-auto"
+            />
+          </div>
+          <div className="mt-4 text-2xl lg:text-4xl max-w-sm text-center font-bold text-pink-400">
+            Redefining Restroom Solutions
+          </div>
+        </div>
+        <div
+          className="mt-8 lg:mt-0 lg:ml-8 text-center lg:text-left"
           data-aos="fade-left"
         >
-          <Image
-            src="/logo.png"
-            width={300}
-            height={300}
-            alt="Logo"
-            className="mx-auto"
-          />
-          <p className="mt-2 text-xl font-semibold max-w-md text-[#B52F73]">
-            Redefining Restroom Solutions
-          </p>
-        </div>
-        <div className="text-center max-w-xl" data-aos="fade-left">
-          <h2 className="text-4xl font-bold">About Us</h2>
-          <p className="mt-4 text-lg">
-            CleanShet&apos;s aims to improve public washroom hygiene for both
-            men and women, enhancing overall well-being and future
-            opportunities.
+          <h2 className="text-3xl lg:text-5xl font-bold mb-4 text-[#331846] text-center mt-10">
+            About Us
+          </h2>
+          <p className="text-xl lg:text-2xl font-semibold max-w-xl mt-4 lg:mt-10">
+            Cleanshet aims to improve public washroom hygiene for both men and
+            women, enhancing overall well-being and future opportunities.
           </p>
         </div>
       </div>
@@ -47,4 +46,4 @@ const MissionStatement: React.FC = () => {
   );
 };
 
-export default MissionStatement;
+export default WhyUs;
